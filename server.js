37fc153431755d5d7afcc118300966e684663131
Glitch (hello-express) <none>
2023-10-15 16:32:42 +0000

    💁🍶 Checkpoint ./server.js:62140568/3390 ./package.json:62140568/3040

--- server.js
+++ server.js
@@ -11,33 +11,34 @@ app.use(express.static("public"));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
-app.post("/WEBHOOK", function (request, response) {
-  
+app.post("/webhook", function (request, response) {
   var connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DB
+    database: process.env.MYSQL_DB,
   });
   connection.connect();
-  
+
   var intentName = request.body.queryResult.intent.displayName;
-  
-  
-  if(intentName == 'AddContatos'){
-    
-    var NomeContato = request.body.queryResult.parameters['Nome'];
-    var CPFContato = request.body.queryResult.parameters['CPF'];
-    var query = 'insert into cadastro values ("'+NomeContato+'","'+CPFContato+'")';
-    
-    connection.query(query, function(erros, results, fields) {
-      if (erros) throw error;
+
+  if (intentName == "AddContatos") {
+    var NomeContato = request.body.queryResult.parameters["Nome"];
+    var CPFContato = request.body.queryResult.parameters["CPF"];
+    var query =
+      'insert into cadastro values ("' +
+      NomeContato +
+      '","' +
+      CPFContato +
+      '")';
+
+    connection.query(query, function (error, results, fields) {
+      if (error) throw error;
       connection.end();
-    })
-    
+      response.json({ "fulfillmentText": "Cadastro realizado com sucesso" });
+    });
   }
-  
-  
+
   const agent = new WebhookClient({ request: request, response: response });
 
   let intentMap = new Map();
