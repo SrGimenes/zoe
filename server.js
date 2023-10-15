d9dcbf53472e4ba0201459b696b85fade44cb97f
Glitch (hello-express) <none>
2023-10-15 14:47:40 +0000

    🚟🐟 Checkpoint ./package.json:62140568/127 ./server.js:62140568/2482

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require("express");
 const app = express();
+const { WebhookClient } = require("dialogflow-fulfillment");
+const bodyParser = require("body-parser");
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
+app.post("/WEBHOOK", function(request, response) {})
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+
+const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
