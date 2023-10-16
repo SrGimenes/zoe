2a41fcc9fb2b553944c04bded282a7b72672f216
Glitch (hello-express) <none>
2023-10-16 04:26:59 +0000

    🐽🌚 Checkpoint ./server.js:62140568/9843

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -24,7 +23,7 @@ app.post("/webhook", function (request, response) {
   connection.connect();
 
   var intentName = request.body.queryResult.intent.displayName;
-  
+
   if (intentName == "Usuario") {
     console.log("Adicionar Contato");
 
@@ -34,25 +33,51 @@ app.post("/webhook", function (request, response) {
     var CPFContato = connection.escape(
       request.body.queryResult.parameters["CPF"]
     );
-    
-    var query =
-      "INSERT INTO Cadastro (Nome, CPF) VALUES (" +
-      NomeContato +
-      ", " +
-      CPFContato +
-      ")";
 
-    connection.query(query, function (error, results, fields) {
+    // Consultar se o CPF já existe na base de dados
+    var queryVerificarCPF =
+      "SELECT CPF FROM Cadastro WHERE CPF = " + CPFContato;
+
+    connection.query(queryVerificarCPF, function (error, results, fields) {
       if (error) {
-        console.error("Erro ao inserir no banco de dados:", error);
-        response.json({ fulfillmentText: "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores." });
+        console.error("Erro ao verificar o CPF no banco de dados:", error);
+        response.json({
+          fulfillmentText:
+            "Erro ao verificar o CPF, por favor entre em contato com os desenvolvedores.",
+        });
       } else {
-        console.log("Usuário cadastrado com sucesso.");
-        response.json({ fulfillmentText: "Usuário cadastrado com sucesso!" });
-      }
-    });
+        if (results.length > 0) {
+          // CPF já existe na base de dados, retornar mensagem informando que o CPF já está cadastrado
+          response.json({
+            fulfillmentText: "CPF já cadastrado na base de dados.",
+          });
+        } else {
+          // CPF não existe na base de dados, continuar com a inserção
+          var query =
+            "INSERT INTO Cadastro (Nome, CPF) VALUES (" +
+            NomeContato +
+            ", " +
+            CPFContato +
+            ")";
 
-    connection.end(); // Feche a conexão após a execução da consulta.
+          connection.query(query, function (error, results, fields) {
+            if (error) {
+              console.error("Erro ao inserir no banco de dados:", error);
+              response.json({
+                fulfillmentText:
+                  "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores.",
+              });
+            } else {
+              console.log("Usuário cadastrado com sucesso.");
+              response.json({
+                fulfillmentText: "Usuário cadastrado com sucesso!",
+              });
+            }
+          });
+        }
+      }
+      connection.end(); // Feche a conexão após a execução da consulta.
+    });
   }
 });
 
