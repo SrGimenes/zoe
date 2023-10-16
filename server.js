a581ee335ad5fc9322dc005541964d234317605d
Glitch (hello-express) <none>
2023-10-16 03:18:35 +0000

    😄🏦 Checkpoint ./server.js:62140568/15402

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,6 +11,17 @@ app.use(express.static("public"));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
+
+function validarCPF(agent) {
+  const cpfPattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+  const userInput = agent.parameters.cpf;
+
+  if (cpfPattern.test(userInput)) {
+    agent.add("CPF válido. O que mais posso fazer por você?");
+  } else {
+    agent.add("Por favor, insira um CPF válido no formato xxx.xxx.xxx-xx.");
+  }
+}
 
 app.post("/webhook", function (request, response) {
   var connection = mysql.createConnection({
@@ -28,12 +38,15 @@ app.post("/webhook", function (request, response) {
   if (intentName == "Usuario") {
     console.log("Adicionar Contato");
 
+    var isCPFValid = 
+    
     var NomeContato = connection.escape(
       request.body.queryResult.parameters["Nome"]
     );
     var CPFContato = connection.escape(
       request.body.queryResult.parameters["CPF"]
     );
+
     var query =
       "INSERT INTO Cadastro (Nome, CPF) VALUES (" +
       NomeContato +
@@ -44,7 +57,10 @@ app.post("/webhook", function (request, response) {
     connection.query(query, function (error, results, fields) {
       if (error) {
         console.error("Erro ao inserir no banco de dados:", error);
-        response.json({ fulfillmentText: "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores." });
+        response.json({
+          fulfillmentText:
+            "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores.",
+        });
       } else {
         console.log("Usuário cadastrado com sucesso.");
         response.json({ fulfillmentText: "Usuário cadastrado com sucesso!" });
