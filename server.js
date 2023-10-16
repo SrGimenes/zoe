7337064a6a3c1fb176115a10122a243bd3c6a8c8
Glitch (hello-express) <none>
2023-10-16 02:55:38 +0000

    🦂🎏 Checkpoint ./server.js:62140568/3956

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -18,7 +19,7 @@ app.post("/webhook", function (request, response) {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
     database: process.env.MYSQL_DB,
-    });
+  });
 
   connection.connect();
 
@@ -27,9 +28,18 @@ app.post("/webhook", function (request, response) {
   if (intentName == "AddContatos") {
     console.log("Adicionar Contato");
 
-    var NomeContato = connection.escape(request.body.queryResult.parameters["Nome"]);
-    var CPFContato = connection.escape(request.body.queryResult.parameters["CPF"]);
-    var query = 'INSERT INTO Cadastro (Nome, CPF) VALUES (' + NomeContato + ', ' + CPFContato + ')';
+    var NomeContato = connection.escape(
+      request.body.queryResult.parameters["Nome"]
+    );
+    var CPFContato = connection.escape(
+      request.body.queryResult.parameters["CPF"]
+    );
+    var query =
+      "INSERT INTO Cadastro (Nome, CPF) VALUES (" +
+      NomeContato +
+      ", " +
+      CPFContato +
+      ")";
 
     connection.query(query, function (error, results, fields) {
       if (error) {
@@ -44,18 +54,6 @@ app.post("/webhook", function (request, response) {
     connection.end(); // Feche a conexão após a execução da consulta.
   }
 });
-
-
-  //const agent = new WebhookClient({ request: request, response: response });
-
-  //let intentMap = new Map();
-  //intentMap.set("AddContatos", FUNCTION);
-  //agent.handleRequest(intentMap);
-
-  //function FUNCTION(agent) {
-  //  agent.add("Isso é um teste");
-  //}
-
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
