1e7730bd8679d01c3a65df6ede09a09925059e18
Glitch (hello-express) <none>
2023-10-16 03:52:39 +0000

    🍗🏉 Checkpoint ./server.js:62140568/29562

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -11,17 +12,6 @@ app.use(express.static("public"));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
-
-// Importe a função de validação
-function validarCPF(agent) {
-  const cpfPattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-  const userInput = agent.parameters.CPF;
-
-  if (cpfPattern.test(userInput)) {
-    return true;
-  }
-  return false;
-}
 
 app.post("/webhook", function (request, response) {
   var connection = mysql.createConnection({
@@ -34,44 +24,37 @@ app.post("/webhook", function (request, response) {
   connection.connect();
 
   var intentName = request.body.queryResult.intent.displayName;
-
+  
   if (intentName == "Usuario") {
     console.log("Adicionar Contato");
 
-    // Validação do CPF
-    var isCPFValid = validarCPF(request.body.queryResult);
-
-    if (!isCPFValid) {
-      response.json({
-        fulfillmentText:
-          "Por favor, insira um CPF válido no formato xxx.xxx.xxx-xx.",
-      });
-      connection.end(); // Feche a conexão após a resposta.
-      return;
-    }
-
-    var NomeContato = connection.escape(request.body.queryResult.parameters["Nome"]);
-    var CPFContato = connection.escape(request.body.queryResult.parameters["CPF"]);
-
+    var NomeContato = connection.escape(
+      request.body.queryResult.parameters["Nome"]
+    );
+    var CPFContato = connection.escape(
+      request.body.queryResult.parameters["CPF"]
+    );
+    
     var query =
-      "INSERT INTO Cadastro (Nome, CPF) VALUES (" + NomeContato + ", " + CPFContato + ")";
+      "INSERT INTO Cadastro (Nome, CPF) VALUES (" +
+      NomeContato +
+      ", " +
+      CPFContato +
+      ")";
 
     connection.query(query, function (error, results, fields) {
       if (error) {
         console.error("Erro ao inserir no banco de dados:", error);
-        response.json({
-          fulfillmentText:
-            "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores.",
-        });
+        response.json({ fulfillmentText: "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores." });
       } else {
         console.log("Usuário cadastrado com sucesso.");
         response.json({ fulfillmentText: "Usuário cadastrado com sucesso!" });
       }
-      connection.end(); // Feche a conexão após a execução da consulta.
     });
+
+    connection.end(); // Feche a conexão após a execução da consulta.
   }
 });
-
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
