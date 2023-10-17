2b048a001ed34966ae8a2a34e74c47740af9cf4f
Glitch (hello-express) <none>
2023-10-17 17:14:12 +0000

    🐣⛄️ Checkpoint ./server.js:62140568/33

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post("/webhook", function (req, res) {
             fulfillmentText: "CPF já cadastrado na base de dados.",
           });
         } else {
-          const query = `INSERT INTO Cadastro (Nome, CPF) VALUES (?, ?)`;
+          const query = `INSERT INTO Cadastro (Nome, CPF) VALUES (${NomeContato}, ${CPFContato})`;
 
           connection.query(query, [NomeContato, CPFFormatado], function (
             error,
