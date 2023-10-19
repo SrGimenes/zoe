b261b65771ed3af698caadbce08d0936ba8300c1
Glitch (hello-express) <none>
2023-10-19 17:08:38 +0000

    🌠🐧 Checkpoint ./server.js:62140568/16247

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const { WebhookClient } = require("dialogflow-fulfillment");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
+const { z } = require('zod');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true });
 app.use(express.static("public"));
 
 app.get("/", function (request, response) {
@@ -29,62 +30,70 @@ app.post("/webhook", function (req, res) {
 
   const intentName = req.body.queryResult.intent.displayName;
 
-  function formatarCPF(CPF) {
-    return CPF.replace(/\D/g, "") // Remove tudo que não é dígito
-      .replace(/(\d{3})(\d)/, "$1.$2") // Coloca ponto entre o terceiro e o quarto dígitos
-      .replace(/(\d{3})(\d)/, "$1.$2") // Coloca ponto entre o sexto e o sétimo dígitos
-      .replace(/(\d{3})(\d{1,2})$/, "$1-$2"); // Coloca hífen entre o nono e o décimo primeiro dígitos
-  }
+  const cpfSchema = z.string()
+    .refine((cpf) => /^[0-9]{11}$/.test(cpf), {
+      message: 'CPF inválido. Deve conter 11 dígitos numéricos.',
+    })
+    .refine((cpf) => !/[a-zA-Z]/.test(cpf), {
+      message: 'CPF inválido. Não deve conter letras.',
+    });
 
   if (intentName === "Default Welcome Intent - yes - yes - yes - yes - next") {
     const NomeContato = req.body.queryResult.parameters["Nome"];
     let CPFContato = req.body.queryResult.parameters["CPF"];
 
-    let CPFFormatado = formatarCPF(CPFContato);
+    try {
+      cpfSchema.parse(CPFContato); // Valida o CPF
 
-    const queryVerificarCPF = `SELECT CPF FROM Cadastro WHERE CPF = ?`;
+      const queryVerificarCPF = `SELECT CPF FROM Cadastro WHERE CPF = ?`;
 
-    connection.query(
-      queryVerificarCPF,
-      [CPFFormatado],
-      function (error, results, fields) {
-        if (error) {
-          console.error("Erro ao verificar o CPF no banco de dados:", error);
-          return res.json({
-            fulfillmentText:
-              "Erro ao verificar o CPF, por favor entre em contato com os desenvolvedores.",
-          });
-        } else {
-          if (results.length > 0) {
+      connection.query(
+        queryVerificarCPF,
+        [CPFContato],
+        function (error, results, fields) {
+          if (error) {
+            console.error("Erro ao verificar o CPF no banco de dados:", error);
             return res.json({
-              fulfillmentText: "CPF já cadastrado na base de dados.",
+              fulfillmentText:
+                "Erro ao verificar o CPF, por favor entre em contato com os desenvolvedores.",
             });
           } else {
-            const query = `INSERT INTO Cadastro (Nome, CPF) VALUES (?, ?)`;
+            if (results.length > 0) {
+              return res.json({
+                fulfillmentText: "CPF já cadastrado na base de dados.",
+              });
+            } else {
+              const query = `INSERT INTO Cadastro (Nome, CPF) VALUES (?, ?)`;
 
-            connection.query(
-              query,
-              [NomeContato, CPFFormatado],
-              function (error, results, fields) {
-                if (error) {
-                  console.error("Erro ao inserir no banco de dados:", error);
-                  return res.json({
-                    fulfillmentText:
-                      "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores.",
-                  });
-                } else {
-                  console.log("Usuário cadastrado com sucesso.");
-                  return res.json({
-                    fulfillmentText: "Usuário cadastrado com sucesso!",
-                  });
+              connection.query(
+                query,
+                [NomeContato, CPFContato],
+                function (error, results, fields) {
+                  if (error) {
+                    console.error("Erro ao inserir no banco de dados:", error);
+                    return res.json({
+                      fulfillmentText:
+                        "Erro ao cadastrar o usuário, por favor entre em contato com os desenvolvedores.",
+                    });
+                  } else {
+                    console.log("Usuário cadastrado com sucesso.");
+                    return res.json({
+                      fulfillmentText: "Usuário cadastrado com sucesso!",
+                    });
+                  }
                 }
-              }
-            );
+              );
+            }
           }
+          connection.end();
         }
-        connection.end();
-      }
-    );
+      );
+    } catch (error) {
+      console.error('CPF inválido:', error.message);
+      return res.json({
+        fulfillmentText: `CPF inválido: ${error.message}`,
+      });
+    }
   }
 });
 
